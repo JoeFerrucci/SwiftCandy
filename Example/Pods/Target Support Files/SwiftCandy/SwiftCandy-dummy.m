b90b988cbed5855744854a6bc58d474a3f646a0c
Joe Ferrucci <ahoy@joeferrucci.info>
2016-05-24 14:50:25 -0700

    initial commit.

---
+++ Example/Pods/Target Support Files/SwiftCandy/SwiftCandy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftCandy : NSObject
+@end
+@implementation PodsDummy_SwiftCandy
+@end
---
+++ Example/Pods/Target Support Files/SwiftCandy/SwiftCandy-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftCandyVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftCandyVersionString[];
+
